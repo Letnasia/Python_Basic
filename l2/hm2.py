c064d5269cc2c5733aed0baae7f90c898bbb8658
Nastya <nasya216@ukr.net>
2023-07-22 19:17:11 +0300

    hw3

--- l2/hm2.py
+++ l2/hm2.py
@@ -1,11 +1,11 @@
 x = int(input('Введіть 4-значне число: '))
 a = x // 1000
-print(a)
 left, right = divmod(x, 100)
 b = left % 10
-print(b)
 left, right = divmod(x,  100)
 c = right // 10
-print(c)
 left, d = divmod(x, 10)
+print(a)
+print(b)
+print(c)
 print(d)
