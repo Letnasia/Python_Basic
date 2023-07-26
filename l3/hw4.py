eabc58baf25d43a3b1e5bd6083a43d853bbe83d2
Nastya <nasya216@ukr.net>
2023-07-26 14:57:35 +0300

    hw4

---
+++ l3/hw4.py
@@ -0,0 +1,16 @@
+x = float(input('Введіть число а:'))
+y = float(input('Введіть число b:'))
+z = input('Введіть оператор для цих чисел:')
+if z == '+':
+    print(x + y)
+elif z == '-':
+    print(x - y)
+elif z == '*':
+    print(x * y)
+elif z == '/':
+    if y != 0:
+        print(x / y)
+    else:
+        print('Помилка. Дільник не може дорівнювати 0.')
+else:
+    print('Помилка. Невідомий оператор.')
