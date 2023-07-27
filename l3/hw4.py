3f3d9dad81a816f7a933cc14677d1fadadd4f197
Nastya <nasya216@ukr.net>
2023-07-27 14:44:26 +0300

    hw6

--- l3/hw4.py
+++ l3/hw4.py
@@ -1,5 +1,5 @@
-x = float(input("Введіть число а:"))
-y = float(input("Введіть число b:"))
+x = float(input("Введіть число: "))
+y = float(input("Введіть число: "))
 z = input("Введіть оператор для цих чисел:")
 if z == '+':
     print(x + y)
