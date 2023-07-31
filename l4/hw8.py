ea7cad8bd88658775c318ef3b8bdd802b8dba70e
Nastya <nasya216@ukr.net>
2023-07-31 11:20:30 +0300

    hw8

--- l4/hw8.py
+++ l4/hw8.py
@@ -2,11 +2,9 @@
 # [6] => 36
 # [] => 0
 
-# lst = [1, 3, 5]
 # lst = [6]
 # lst = []
-
-lst = [56, 6, 8, 7, 12, 8]
+lst = [1, 3, 5]
 summa = 0
 if len(lst) != 0:
     for i in range(0, len(lst), 2):
