7b76df7c64545a1c6610e02991fd9b194df4d34f
Nastya <nasya216@ukr.net>
2023-08-06 16:59:36 +0300

    hw14

---
+++ l6/hw14.py
@@ -0,0 +1,13 @@
+# 1000 -> 0
+# 423 -> 8
+# 1 -> 1
+
+dig = int(input('Enter integer number: '))
+
+while dig > 10:
+    dig_str = str(dig)
+    dig = 1
+    for val in dig_str:
+        val = int(val)
+        dig *= val
+print(dig)
